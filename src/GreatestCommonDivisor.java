9fd273ba35865848306acf05aa37be96bf1e8fbc
ssmith34 <110691416+ssmith34@users.noreply.github.com>
2022-09-13 13:46:14 -0400

    Euclidian Algorithm implemented

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -1,6 +1,19 @@
 public class GreatestCommonDivisor {
 
     public static int getGreatestCommonDivisor (int first, int second) {
+        // Edge Cases
+        if (first < 10 || second < 10) {
+            return -1;
+        }
 
+        while (first != second) {
+            if (first > second) {
+                first = first - second;
+            }
+            else {
+                second = second = first;
+            }
+        }
+        return second;
     }
 }
